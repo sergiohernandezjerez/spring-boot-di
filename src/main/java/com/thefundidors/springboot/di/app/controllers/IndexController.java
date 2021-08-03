742a772d1c84e580d030ac17e7be28cd3523c2cf
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-08-03 18:57:05 +0200

    Inyectando con Qualifier

--- src/main/java/com/thefundidors/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/thefundidors/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.thefundidors.springboot.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/thefundidors/springboot/di/app/models/service/MiServicioComplejo.java
+++ src/main/java/com/thefundidors/springboot/di/app/models/service/MiServicioComplejo.java
@@ -1,6 +1,5 @@
 package com.thefundidors.springboot.di.app.models.service;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component("miServicioComplejo")
