830143b152985e46af438aa2cd3ec0d401264f79
Sergio Hernández Jerez <sergiohernandezjerez@gmail.com>
2021-08-03 18:17:08 +0200

    Primera inyección de dependencias

---
+++ src/main/java/com/thefundidors/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,13 @@
+package com.thefundidors.springboot.di.app.models.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MiServicio {
+	
+	
+	public String operacion() {
+		return "ejecutando algún proceso importante...";
+	}
+
+}
